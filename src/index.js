7e3bfe698f0efad71be4e8d40586bebd0ac75dd3
donald <donald@maana.io>
2018-06-20 07:50:48 -0700

    it works without GPU

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-export default (input = 'No args passed!') => input;
+const tf = require('@tensorflow/tfjs');
+
+// Load the binding:
+const tfNode = require('@tensorflow/tfjs-node');
+// Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+console.log('tfjs-node: ', tfNode);
+
+// Set the backend to TensorFlow:
+tf.setBackend('tensorflow');
+
+// Train a simple model:
+const model = tf.sequential();
+model.add(
+  tf.layers.dense({ units: 100, activation: 'relu', inputShape: [10] })
+);
+model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
+model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: async (epoch, log) => {
+      console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+    }
+  }
+});
